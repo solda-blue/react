c7c03d331957fdf443633bc940be01e18858cd75
solda <principe1513@gmail.com>
2023-03-29 18:19:46 +0900

    test

---
+++ first-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>리액트 프로젝트를 수정해서 사용합니다</h1>
+    </div>
+  );
+}
+
+export default App;
